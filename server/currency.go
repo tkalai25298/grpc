e287cb24e5c6788fc452c29261e1d2d8e80df5f5
kalaiyarasi-ngpws <kalaiyarasi@ngpwebsmart.com>
2021-06-10 21:14:51 +0530

    added bi-dir streaming

--- server/currency.go
+++ server/currency.go
@@ -12,11 +12,46 @@ import (
 // Currency is a gRPC server it implements the methods defined by the CurrencyServer interface
 type Currency struct {
 	log hclog.Logger
+	subscriptions map[protos.Currency_GetStreamRateServer][]*protos.RateRequest //maintaining different clients & sending resp to particular client request
 }
 
 // NewCurrency creates a new Currency 
 func NewCurrency(l hclog.Logger) *Currency {
-	return &Currency{l}
+
+	c := &Currency{l,make(map[protos.Currency_GetStreamRateServer][]*protos.RateRequest)}
+	go c.handleSubscriptions()
+
+	return c
+}
+
+
+//sending subscriptions for every 5seconds
+func (c *Currency) handleSubscriptions() {
+	
+		for { //continously sending server rates to clients
+			c.log.Info("server sending rates")
+
+
+			// loop over subscribed clients
+			for k, v := range c.subscriptions {
+
+				// loop over subscribed rates
+				for _, rr := range v {
+					r, err := c.GetRate(context.Background(),rr)
+					if err != nil {
+						c.log.Error("Unable to get update rate", "base", rr.Base, "destination", rr.Destination)
+					}
+	
+					err = k.Send(&protos.RateResponse{Base: rr.Base, Destination: rr.Destination, Rate: r.Rate})
+					if err != nil {
+						c.log.Error("Unable to send updated rate", "base", rr.Base, "destination", rr.Destination)
+					}
+					
+				}
+			}
+			time.Sleep(5*time.Second)
+		}
+	
 }
 
 // GetRate implements the CurrencyServer GetRate method and returns the currency exchange rate
@@ -29,7 +64,7 @@ func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos
 func (c *Currency) GetStreamRate(str protos.Currency_GetStreamRateServer) error {
 
 	//using go func to simultaneously handle client and server
-	go func ()  {
+	// go func ()  {
 
 		//receiving client stream 
 		//looping over to receive continous requests
@@ -48,15 +83,20 @@ func (c *Currency) GetStreamRate(str protos.Currency_GetStreamRateServer) error 
 				break
 			}
 			c.log.Info("Handling client requests","request",rr)
-		}
-	}()
 
-	//sending server stream msg
-	for {
-		err := str.Send(&protos.RateResponse{Rate: 25})
-		if err != nil {
-			return err
+			//caching the client requests to subscriptions map 
+			//creating subscriptions and adding the list of each rateRequests from client to the server streaming
+			rrs,ok := c.subscriptions[str]
+			//if the key doesnt exists create new 
+			if !ok {
+				rrs = []*protos.RateRequest{}
+			}
+
+			//appending all the client requests made
+			rrs = append(rrs, rr)
+			c.subscriptions[str] = rrs
 		}
-		time.Sleep(5 *time.Second)
-	}
+	// }()
+
+	return nil
 } 
